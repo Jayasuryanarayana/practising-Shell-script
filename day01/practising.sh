241cbf49369a3d6343160730cd7c137d45fdc201
Jayasuryanarayana <157093030+Jayasuryanarayana@users.noreply.github.com>
2025-06-30 11:40:44 +0530

    practising.sh

---
+++ day01/practising.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Just a short notes on what i've learned today
+
+echo "i have went through instruction of 7-day shell scripting"
+echo "Now i have completed one video on it"
+echo " I understood what is a shell, it interacts with kernel , why do we use shell(To automate things)"
+echo "shell was placed in abinary folder /bin/sh"
+echo "shell has different improvisations(Most popular one which we use is BASH(Bourne Again Shell))"
+echo "used mkdir to create a hello.sh file"
+echo "To check permissions of a file command= ls -l "
+echo " to give read/write/execute permission i have to give command chmod 754 hello.sh"
+echo "chmod makes changes in file permissions"
+echo "7=read, write,execute , 5=read, execute, 4=read"
+echo "if the output of ls -l starts from d them its a directory if it starts from "-" its a file "
+echo "if the color of the file is green , then its an executable file, if its white=Normal file , if its a blue file = directory"
+echo "After making a file executable , if we wish to execute it , we can use ./hello.sh to execute the file "
